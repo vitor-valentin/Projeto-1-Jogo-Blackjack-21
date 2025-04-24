fd2d394a61cbebdaf8b3aa205374ada70faaa5e2
vitor-valentin <vitor.rohling.becker@gmail.com>
2025-04-24 18:18:23 -0300

    - Finished html of main menu - Play button opening game section - Close button closing the game section - Add of 2 fonts - Addition of many images of styles of card backs - Addition of image of chips to the menu

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const bgMusic = document.getElementById("bgMusic");
+const playBtn = document.getElementById("play");
+const rulesBtn = document.getElementById("rules");
+const configBtn = document.getElementById("config");
+const gamePage = document.querySelector(".game");
+const rulesPage = document.querySelector(".rules");
+const configPage = document.querySelector(".config");
+const closeGameBtn = document.getElementById("closeGame");
+
+
+playBtn.addEventListener("click", () => {
+    gamePage.style.width = "100%";
+});
+
+closeGameBtn.addEventListener("click", () => {
+    gamePage.style.width = "0";
+});
